ddf867ef86bf7e5449fc26e19e8d5ea7906e4761
Евгений Кучма <desferto87@gmail.com>
2024-06-19 22:30:06 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkNineSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberStart = 5;
+            int risingNumber = 7;
+            int numberMax = 103;
+
+            while (numberStart <= numberMax)
+            {
+                Console.WriteLine(numberStart);
+                numberStart += risingNumber;
+            }
+        }
+    }
+}
